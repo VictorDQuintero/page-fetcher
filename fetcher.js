e66982e273a96c5d8fcaf4e3b8a2bc86f41b1623
Victor Quintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-13 21:26:42 -0600

    Add code, fetcher function now gets response.body and fs.writeFile writes the html code in the user specified file, then printOutSizePath prints byte size and file path

--- fetcher.js
+++ fetcher.js
@@ -5,28 +5,24 @@ const URL = process.argv[2];
 const filePath = process.argv[3];
 
 const fetcher = function (url, path, callback) {
-  // whenDone is a callback function
   console.log("fetcher doing its thing...");
 
   needle.get(url, (error, body) => {
     if (!error) {
-      callback(path, body);
+      fs.writeFile(path, body.body, "utf8", (error, data) => {
+        console.log("In writeFile's Callback: it has the data.");
+
+        if (!error) {
+          data = body;
+          callback(data, path);
+        }
+      });
     }
   });
 };
 
-const writer = function (path, body, callback) {
-  fs.writeFile(path, "utf8", (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
-
-    if (!error) {
-      callback(data); // whenDone takes data and because printOutCarBreed is in its place when calling breedDetailsFromFile(), printOut...() gets data and prints it
-    }
-  });
+const printOutSizePath = (data, path) => {
+  console.log(`Downloaded and saved ${data.bytes} bytes to ${path}`);
 };
 
-const printOutResult = (data) => {
-  console.log("Return Value: ", data);
-};
-
-printOutResult(fetcher(URL, filePath, writer));
+fetcher(URL, filePath, printOutSizePath);
